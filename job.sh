6ce04cfb083480bccf3e12ed0bb95faabac863ef
ekinaykurek <akyurekekin@gmail.com>
2018-08-21 19:17:46 +0300

    self attn and gating tested 98.65 accuracy is obtanied

--- job.sh
+++ job.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #SBATCH --partition=ai
 #SBATCH --gres=gpu:1
-#SBATCH --time=48:00:00
+#SBATCH --time=56:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=96GB
+#SBATCH --mem-per-cpu=100GB
 #SBATCH --job-name="clevr"
 #SBATCH --output=clevr.out
 #SBATCH --error=clevr.error
 
-julia train.jl
+julia train.jl src/macnet.jl configs/config2.jl
