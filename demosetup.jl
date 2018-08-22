8be1fc0db59590ceeb67f7e08ad6b6ac1746d0bf
ekinakyurek <akyurekekin@gmail.com>
2018-08-22 10:20:02 -0400

    use println instead of info

--- demosetup.jl
+++ demosetup.jl
@@ -8,12 +8,12 @@
 # end
 server="ai.ku.edu.tr/"
 if !isdir("data/demo")
-    info("Downloading sample questions and images from CLEVR dataset...")
+    println("Downloading sample questions and images from CLEVR dataset...")
     download(server*"data/mac-network/demo.tar.gz","demo.tar.gz")
     run(`tar -xzf demo.tar.gz`)
     rm("demo.tar.gz")
 end
 if !isfile("models/macnet.jld2")
-    info("Downloading pre-trained model from our servers...")
+    println("Downloading pre-trained model from our servers...")
     download(server*"models/mac-network/macnet.jld2","models/macnet.jld2")
 end
